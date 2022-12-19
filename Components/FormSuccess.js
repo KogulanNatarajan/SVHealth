283cefdf4a9ed58dbf738eda362a4430c3063389
KogulanNatarajan <110106857+KogulanNatarajan@users.noreply.github.com>
2022-12-19 11:26:32 +0100

    Add files via upload

---
+++ Components/FormSuccess.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { Text,View,ActivityIndicator,StyleSheet , Image,TouchableOpacity } from "react-native";
+import {Overlay} from 'react-native-elements';
+
+
+const FormSuccess = (props)=>{
+
+    return(
+        props.successMessage?
+            <Overlay overlayStyle={styles.Overlay} isVisible={true} onBackdropPress={()=>props.close()}>
+            <Image 
+            style={styles.logostyle}
+            source={require('../Assets/Images/check.png')}/>
+
+            <Text style={styles.successMessage}>
+            {props.successMessage}
+             </Text>
+
+            {/* <TouchableOpacity onPress={()=>props.hideErrorOverlay(false)} style={styles.Button}>
+                <Text style={styles.ButtonText}>
+                    Okay
+                </Text>
+             </TouchableOpacity> */}
+            </Overlay>
+        :
+        <Overlay isVisible={true}>
+                <ActivityIndicator size={'large'}/>
+            </Overlay>
+
+        
+        
+        
+    )
+ 
+}
+
+
+const styles = StyleSheet.create({
+    Overlay:{
+        width:'50%',
+        height:'30%',
+        borderRadius: 30,
+        //opacity:0.9,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center'
+
+    },
+    logostyle:{
+
+        width:72,
+        height:72
+    },
+    successMessage:{
+        marginTop: 10
+
+
+    },
+    Button:{
+        width:'80%',
+        backgroundColor:'#000000',
+        height:52,
+        borderRadius: 15,
+        paddingLeft: 15,
+        marginTop:20,
+        color:'#000000',
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    ButtonText:{
+        fontSize:16,
+        fontWeight:'bold',
+        color:'#ffffff'
+    }
+
+
+})
+export default FormSuccess
